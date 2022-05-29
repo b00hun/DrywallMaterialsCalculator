18c7e8d0c19cf7b93c8925f88a276f9d6b823017
Bartosz Sadowski <bshadowsky@gmail.com>
2022-05-29 21:06:23 +0200

    Added WallType controller

---
+++ DrywallCalculator/DrywallCalculator/Controllers/WallTypeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrywallCalculator.Controllers
+{
+    public class WallTypeController : Controller
+    {
+        // GET: WallTypeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        // GET: WallTypeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WallTypeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+ 
+
+        // GET: WallTypeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WallTypeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- DrywallCalculator/DrywallCalculator/Program.cs
+++ DrywallCalculator/DrywallCalculator/Program.cs
@@ -7,12 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<IWallTypeRepository, WallTypeRepository>();
 builder.Services.AddScoped<IDrywallRepository, DrywallRepository>();
 builder.Services.AddScoped<IWallTypeService, WallTypeService>();
 builder.Services.AddScoped<IDrywallService, DrywallService>();
-builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
